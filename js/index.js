9e7160ae819cacea580f1a69b09cb80a861c84a6
Lminxia <lmxbaymax@gmial.com>
2018-06-14 12:14:59 +0800

    change enter and registered

--- js/index.js
+++ js/index.js
@@ -11,3 +11,62 @@ function less(y){
 	document.getElementById("mo"+y).style.display="block";	
 	document.getElementById("le"+y).style.display="none";
 }
+
+$(function(){			
+	$('#enter').validate({
+		rules: {
+			user: {
+				required: true,
+				rangelength:[2,10]
+			},
+			enterpassword: {
+				required: true,
+				minlength: 6
+			}
+		}
+	})
+	
+	$("#enrol").validate({
+		rules: {
+			useremail:{
+				required: true,
+				email:true
+			},
+			enrolpassword: {
+				required: true,
+				minlength: 6
+			},
+			comfirmpassword: {
+				required: true,
+				 equalTo: "#enrolpassword"
+			},
+			phone: {
+				required: true,
+				number : true,
+				minlength:11,
+				maxlength:11
+			}
+		}
+//		,
+//		messages: {
+//			useremail:{
+//				required: "请输入邮箱",
+//				email: "请输入正确的邮箱"
+//			},
+//			enrolpassword: {
+//				required: "必须填写密码",
+//				minlength: "用密码长度不能小于6"
+//			},
+//			comfirmpassword: {
+//				required: "请再次输入密码",
+//				equalTo: "两次密码输入不一致"
+//			},
+//			phone: {
+//				required: "请输入手机号码",
+//				rangelength: "请输入11位手机号码"
+//			}
+//		}
+	})
+	
+})
+
